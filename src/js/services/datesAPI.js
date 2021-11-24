3c42b96f5cf53d77c964a0fe26552d9f158013c2
dany rose <danyrose1995@gmail.com>
2021-11-24 10:31:54 +0100

    ajout fonction jours d'une semaine donnée

--- src/js/services/datesAPI.js
+++ src/js/services/datesAPI.js
@@ -3,7 +3,6 @@ function getWeekNumber(date) {
   manipulatedDate = date
     ? new Date(date)
     : new Date(new Date().getFullYear(), 11, 31);
-
   manipulatedDate.setHours(0, 0, 0, 0);
   manipulatedDate.setDate(
     manipulatedDate.getDate() + 3 - ((manipulatedDate.getDay() + 6) % 7)
@@ -20,6 +19,27 @@ function getWeekNumber(date) {
   );
 }
 
-const datesApi = { getWeekNumber };
+function getWeekDays(week) {
+  const januaryFour = new Date(new Date().getFullYear(), 0, 4);
+  const firstDayOfFirstWeek = new Date(januaryFour);
+  firstDayOfFirstWeek.setDate(
+    firstDayOfFirstWeek.getDate() - firstDayOfFirstWeek.getDay() + 1
+  );
+  const manipulatedDate = new Date(firstDayOfFirstWeek);
+  manipulatedDate.setDate(
+    manipulatedDate.getDate() + (week - 1) * 7
+  );
+  const weekDays = [];
+  manipulatedDate.setDate(
+    manipulatedDate.getDate() - manipulatedDate.getDay() + 1
+  );
+  for (var i = 0; i < 7; i++) {
+    weekDays.push(new Date(manipulatedDate));
+    manipulatedDate.setDate(manipulatedDate.getDate() + 1);
+  }
+  return weekDays;
+}
+
+const datesApi = { getWeekNumber, getWeekDays };
 
 export default datesApi;
