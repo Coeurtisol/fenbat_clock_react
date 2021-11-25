d72d1df1517efb204c454b37f0793ee5dc90e311
dany rose <danyrose1995@gmail.com>
2021-11-25 08:21:09 +0100

    method trouver une semaine

--- src/js/services/semainesAPI.js
+++ src/js/services/semainesAPI.js
@@ -5,10 +5,14 @@ async function create(semaine) {
   return await axios.post(SEMAINES_API_URL, semaine);
 }
 
+async function findOne(year, week, userId) {
+  return await axios.get(`${SEMAINES_API_URL}/${year}/${week}/${userId}`);
+}
+
 async function update(id, semaine) {
   return await axios.put(SEMAINES_API_URL + "/" + id, semaine);
 }
 
-const SEMAINES_API = { create, update };
+const SEMAINES_API = { create, update, findOne };
 
 export default SEMAINES_API;
