3532947ac56163394401f4027f78e9f577ca57e7
dany rose <danyrose1995@gmail.com>
2022-02-02 17:07:21 +0100

    add .env

--- src/js/configs/api_links.js
+++ src/js/configs/api_links.js
@@ -1,5 +1,4 @@
-// const API_URL = "http://localhost:4000/api/";
-const API_URL = "http://test1.rosedevelopment.fr:24000/api/";
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const USERS_API_URL = API_URL + "users";
 export const ROLES_API_URL = API_URL + "roles";
