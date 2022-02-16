f1f49dfc86852296b639c8c08f1214d758f69b2f
dany rose <danyrose1995@gmail.com>
2022-02-16 14:25:07 +0100

    style page introuvable

--- src/js/pages/NotFoundPage.jsx
+++ src/js/pages/NotFoundPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <main>
+    <main className="color-text color text-center mt-5">
       <h1>Page introuvable</h1>
+      <Link className="btn btn-primary btn-sm" to="/">Retour à l'accueil</Link>
     </main>
   );
 };
